0d834f74e9be298a3fe592389252a1851ef9749e
nishit <nishit@avdevs.com>
2022-01-10 11:24:03 +0530

    added secureCookie

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,19 +2,23 @@ import { getToken } from 'next-auth/jwt';
 import { NextResponse } from 'next/server';
 
 export async function middleware(req) {
-	// Token will exist if the user is logged in
-	const token = await getToken({ req, secret: process.env.JWT_SECRET });
+    // Token will exist if the user is logged in
+    const token = await getToken({
+        req,
+        secret: process.env.JWT_SECRET,
+        secureCookie: process.env.NEXTAUTH_URL?.startsWith('https://') ?? !!process.env.VERCEL_URL,
+    });
 
-	const { pathname } = req.nextUrl;
+    const { pathname } = req.nextUrl;
 
-	// Allow the request if the following is true
-	// 1. the token exists
-	if (pathname.includes('/api/auth') || token) {
-		return NextResponse.next();
-	}
+    // Allow the request if the following is true
+    // 1. the token exists
+    if (pathname.includes('/api/auth') || token) {
+        return NextResponse.next();
+    }
 
-	// Redirect if token does not exist and they are trying to request a protected route
-	if (!token && pathname !== '/login') {
-		return NextResponse.redirect('/login');
-	}
+    // Redirect if token does not exist and they are trying to request a protected route
+    if (!token && pathname !== '/login') {
+        return NextResponse.redirect('/login');
+    }
 }
